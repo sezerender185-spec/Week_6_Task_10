3b17eb46c022e60e01cbc3d2ee451fe99826fc12
sezerender185-spec <sezerender185@gmail.com>
2025-11-07 00:17:51 +0300

    Add files via upload

---
+++ Week_6_Task_10.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+class Car {
+public:
+    string brand;
+    int year;
+
+};
+int main()
+{
+    ifstream file("data.txt");
+    if (!file) {
+        cout << "Error! " << endl;
+        return;
+
+    }
+    Car cars[100];  
+    int count = 0;
+    while (file >> cars[count].brand >> cars[count].year) {
+        count++;
+    }
+    file.close();
+    for (int i(0); i < count;
+        i++) {
+        cout << "Brand: " << cars[i].brand << "Year: " << cars[i].year << endl;
+    }
+    return 1;
+}
